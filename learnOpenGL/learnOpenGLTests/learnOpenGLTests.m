e3e2aa15c47691a593a638b5c2364a54eed1f75d
parasiter <937247098@qq.com>
2019-03-21 20:01:32 +0800

    add:[env] opengl

---
+++ learnOpenGL/learnOpenGLTests/learnOpenGLTests.m
@@ -0,0 +1,37 @@
+//
+//  learnOpenGLTests.m
+//  learnOpenGLTests
+//
+//  Created by 吴凡 on 2019/3/21.
+//  Copyright © 2019 吴凡. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface learnOpenGLTests : XCTestCase
+
+@end
+
+@implementation learnOpenGLTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
